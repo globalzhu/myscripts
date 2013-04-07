54dd4b577d1664c04d9a13ce8ee4f9d795f2cc54
slackwareer <slackwareer@gmail.com>
2013-04-07 17:19:23 +0800

    Revert "all the scripts"
    This reverts commit d0ea92644fcff80a4e7dad6186980d23d8b31f9e.

--- test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-free=`free -m|grep Mem`
-
-total=`echo $free|awk '{print $2}'`
-free_mem=`echo $free|awk '{print $4}'`
-buffered_mem=`echo $free|awk '{print $6}'`
-cached_mem=`echo $free|awk '{print $7}'`
-echo "$total=free:$free_mem,buffered:$buffered_mem,cached:$cached_mem"
