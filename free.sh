8c30aa029f78206a5ccc13b6ef19c2f41e41b41c
slackwareer <slackwareer@gmail.com>
2013-04-11 15:35:22 +0800

    modify free.sh

--- free.sh
+++ free.sh
@@ -6,4 +6,4 @@ total=`echo $free|awk '{print $2}'`
 free_mem=`echo $free|awk '{print $4}'`
 buffered_mem=`echo $free|awk '{print $6}'`
 cached_mem=`echo $free|awk '{print $7}'`
-echo "|$free_mem,$buffered_mem,$cached_mem|"
+echo "[$free_mem,$buffered_mem,$cached_mem]"
