af8f09211a9c97920192f85513b0defd52632e1c
slackwareer <slackwareer@gmail.com>
2013-04-20 16:19:37 +0800

    password hidden

--- remote-router.sh
+++ remote-router.sh
@@ -4,4 +4,5 @@
 cmd=$(cat remote-script/flush-iptables.sh)
 echo $cmd
 
-sshpass -p 330953127 ssh root@192.168.1.1 "$cmd"
+password=$(fetch_parameter.sh password password)
+sshpass -p $password ssh root@192.168.1.1 "$cmd"
