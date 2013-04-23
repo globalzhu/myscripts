d63a33d152584f1b422d4134fe7100ae3136184f
slackwareer <slackwareer@gmail.com>
2013-04-23 22:47:30 +0800

    openwrts password

--- remote-router.sh
+++ remote-router.sh
@@ -4,5 +4,5 @@
 cmd=$(cat /store/user_scripts/remote-script/flush-iptables.sh)
 echo $cmd
 
-password=$(fetch_parameter.sh /store/user_scripts/password password)
+password=$(fetch_parameter.sh /etc/mypass password)
 sshpass -p $password ssh root@192.168.1.1 "$cmd"
