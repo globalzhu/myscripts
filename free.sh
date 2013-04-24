f600af7d4ecf6aa12618c06fcd450584da1603ee
slackwareer <slackwareer@gmail.com>
2013-04-24 17:27:24 +0800

    modify free script

--- free.sh
+++ free.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-free=`free -m|grep Mem`
+free=$(free -m|grep Mem)
 
-total=`echo $free|awk '{print $2}'`
-free_mem=`echo $free|awk '{print $4}'`
-buffered_mem=`echo $free|awk '{print $6}'`
-cached_mem=`echo $free|awk '{print $7}'`
-echo "[$free_mem,$buffered_mem,$cached_mem]"
+display=$(echo $free|awk '{print $4,$6,$7}')
+echo [$display]
