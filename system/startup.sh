a3d9060554e412b4df96f79c45a9805feea2a82e
slackwareer <slackwareer@gmail.com>
2013-05-07 22:25:58 +0800

    fix

--- system/startup.sh
+++ system/startup.sh
@@ -2,3 +2,4 @@
 password=$(fetch_parameter.sh /etc/mypass password)
 echo $password | sudo -S /store/user_scripts/system/cpu_freq.sh
 echo $password | sudo -S /store/user_scripts/system/ntp.sh
+echo $password | sudo -S /store/user_scripts/system/router.sh
