6625ba8287c8555dfe59ff56537dc2ccd3f19943
slackwareer <slackwareer@gmail.com>
2013-05-26 19:59:11 +0800

    commit

--- system/startup.sh
+++ system/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-password=$(fetch_parameter.sh /etc/mypass password)
+password=$(/store/user_scripts/fetch_parameter.sh /etc/mypass password)
 echo $password | sudo -S /store/user_scripts/system/cpu_freq.sh
 echo $password | sudo -S /store/user_scripts/system/ntp.sh
 echo $password | sudo -S /store/user_scripts/system/router.sh
