3b23e095f5ce088fd3ff8467c108d95941e1be66
slackwareer <slackwareer@gmail.com>
2013-05-26 16:11:14 +0800

    router

--- system/router.sh
+++ system/router.sh
@@ -12,9 +12,8 @@ iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
 iptables --append FORWARD --in-interface wlan0 -j ACCEPT
 
 ##为啥用这个？？？
-#iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 3128
 iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to 10.0.0.1:3128
 
-systemctl restart squid
+#systemctl restart squid
 
 echo "router started"
